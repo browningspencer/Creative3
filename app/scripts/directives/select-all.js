a55a98718557ee50e17ce09c7dc450f9fb5f698b
wjj2329 <wjj2329@gmail.com>
2016-02-22 15:05:45 -0700

    get
    get

--- app/scripts/directives/select-all.js
+++
@@ -1,9 +0,0 @@
-app.directive('selectAll', function () {
-  return {
-    link: function (scope, element, attrs) {
-      element.on('click', function () {
-        this.select();
-      });
-    }
-  };
-});
--- app/scripts/filters/highlight.js
+++
@@ -1,8 +0,0 @@
-app.filter('highlight', ['$sce', function($sce) {
-  return function(text, phrase) {
-    if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
-      '<span class="highlighted">$1</span>')
-
-    return $sce.trustAsHtml(text)
-  }
-}]);
